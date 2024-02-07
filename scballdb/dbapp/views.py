255ffd63f706227ca2e4fd704c566035e911a9da
LavarExpl <96665015+LavarExpl@users.noreply.github.com>
2024-02-06 20:12:11 -0800

    Update views.py

--- scballdb/dbapp/views.py
+++ scballdb/dbapp/views.py
@@ -1,10 +1,14 @@
+from typing import Any
+from django.forms.renderers import BaseRenderer
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django import forms
 
 # Create your views here.
-
+@login_required
 def home(request):
     return render(request, 'dbapp/home.html',{})
 
@@ -30,15 +34,37 @@ def register (request):
 
         new_user = User.objects.create_user(username=username,password=password,email=email)
         new_user.save()
+
         print(f"this users name is {username} and his email is {email} and password was {password}.")
+        messages.success(request,"Your user has been created! You can go log in now!")
+        return redirect('loginp')
     return render(request, "dbapp/register.html",{})
 
-def loginp (request):
-   
-        
-    return render(request, "dbapp/login.html",{})
+ 
 
-def accounts (request):
-   
-        
-    return render(request, "dbapp/accounts.html",{})
+def loginp(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username_or_email = request.POST.get('Username')
+        password = request.POST.get('Password')
+
+        # Assuming username_or_email can be either a username or an email
+        user = authenticate(request, username=username_or_email, password=password)
+
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'NO ACCOUNTS EXIST')
+            return redirect('register')
+
+    return render(request, "dbapp/login.html", {})
+
+
+
+def logout_ (request):
+     logout(request)
+     return redirect('loginp')
